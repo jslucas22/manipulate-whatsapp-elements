6159cf41e62c801fbfc6661ddfefe61b5a84355f
José Lucas <jose.esbellute@gmail.com>
2022-11-17 07:45:07 -0300

    Removed download button when previewing a media item

--- src/js/strategy/strategies/DownloadStrategy.js
+++ src/js/strategy/strategies/DownloadStrategy.js
@@ -1,13 +1,24 @@
 class DownloadStrategy {
     manipulate() {
+        this.tratarSeletorMsgDownload()
         this.tratarSeletorDownload()
     }
 
-    tratarSeletorDownload() {
+    tratarSeletorMsgDownload() {
         var downloadElement = document.querySelectorAll('[data-testid=mi-msg-download]');
         if (downloadElement != null)
             for (let element of downloadElement) {
                 element.remove(element)
+            }
+    }
+
+    tratarSeletorDownload() {
+        var downloadElement = document.querySelectorAll('[data-testid=download]');
+        if (downloadElement != null)
+            for (let element of downloadElement) {
+                element
+                    .parentElement
+                    .remove(element)
             }
     }
 }
